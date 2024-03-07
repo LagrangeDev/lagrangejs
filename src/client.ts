9c56b675a775d7b1067e34dd16cb19f88336eb4e
Linwenxuan <llwwxxllwwxx@gmail.com>
2024-03-07 21:29:21 +0800

    [core] further indentation

--- src/client.ts
+++ src/client.ts
@@ -13,236 +13,236 @@ import { GroupMember } from './entities/groupMember';
 import { LoginErrorCode } from "./errors";
 
 export interface Client extends BaseClient {
-  on<T extends keyof EventMap>(event: T, listener: EventMap<this>[T]): this;
+    on<T extends keyof EventMap>(event: T, listener: EventMap<this>[T]): this;
 
-  on<S extends string | symbol>(
-      event: S & Exclude<S, keyof EventMap>,
-      listener: (this: this, ...args: any[]) => void,
-  ): this;
+    on<S extends string | symbol>(
+        event: S & Exclude<S, keyof EventMap>,
+        listener: (this: this, ...args: any[]) => void,
+    ): this;
 
-  once<T extends keyof EventMap>(event: T, listener: EventMap<this>[T]): this;
+    once<T extends keyof EventMap>(event: T, listener: EventMap<this>[T]): this;
 
-  once<S extends string | symbol>(
-      event: S & Exclude<S, keyof EventMap>,
-      listener: (this: this, ...args: any[]) => void,
-  ): this;
+    once<S extends string | symbol>(
+        event: S & Exclude<S, keyof EventMap>,
+        listener: (this: this, ...args: any[]) => void,
+    ): this;
 
-  prependListener<T extends keyof EventMap>(event: T, listener: EventMap<this>[T]): this;
+    prependListener<T extends keyof EventMap>(event: T, listener: EventMap<this>[T]): this;
 
-  prependListener(event: string | symbol, listener: (this: this, ...args: any[]) => void): this;
+    prependListener(event: string | symbol, listener: (this: this, ...args: any[]) => void): this;
 
-  prependOnceListener<T extends keyof EventMap>(event: T, listener: EventMap<this>[T]): this;
+    prependOnceListener<T extends keyof EventMap>(event: T, listener: EventMap<this>[T]): this;
 
-  prependOnceListener(event: string | symbol, listener: (this: this, ...args: any[]) => void): this;
+    prependOnceListener(event: string | symbol, listener: (this: this, ...args: any[]) => void): this;
 
-  off<T extends keyof EventMap>(event: T, listener: EventMap<this>[T]): this;
+    off<T extends keyof EventMap>(event: T, listener: EventMap<this>[T]): this;
 
-  off<S extends string | symbol>(
-      event: S & Exclude<S, keyof EventMap>,
-      listener: (this: this, ...args: any[]) => void,
-  ): this;
+    off<S extends string | symbol>(
+        event: S & Exclude<S, keyof EventMap>,
+        listener: (this: this, ...args: any[]) => void,
+    ): this;
 }
 
 export class Client extends BaseClient {
-  readonly logger: Logger;
-  readonly directory: string;
-  readonly config: Required<Config>;
-  readonly token: SavedToken;
+    readonly logger: Logger;
+    readonly directory: string;
+    readonly config: Required<Config>;
+    readonly token: SavedToken;
 
-  readonly friendList = new Map<number, Friend.Info>();
-  readonly groupList = new Map<number, Group.Info>();
-  readonly memberList = new Map<number, Map<number, GroupMember.Info>>();
-  get cacheDir() {
-    const dir = path.resolve(this.directory, '../image');
-    if (!fs.existsSync(dir)) fs.mkdirSync(dir);
-    return dir;
-  }
-  pickFriend = Friend.from.bind(this);
-  pickGroup = Group.from.bind(this);
-  pickMember = GroupMember.from.bind(this);
+    readonly friendList = new Map<number, Friend.Info>();
+    readonly groupList = new Map<number, Group.Info>();
+    readonly memberList = new Map<number, Map<number, GroupMember.Info>>();
+    get cacheDir() {
+        const dir = path.resolve(this.directory, '../image');
+        if (!fs.existsSync(dir)) fs.mkdirSync(dir);
+        return dir;
+    }
+    pickFriend = Friend.from.bind(this);
+    pickGroup = Group.from.bind(this);
+    pickMember = GroupMember.from.bind(this);
 
-  constructor(uin: number, conf?: Config) {
-    const config = {
-      logLevel: 'info' as LogLevel,
-      platform: Platform.Linux,
-      autoServer: true,
-      ignoreSelf: true,
-      cacheMember: true,
-      reConnInterval: 5,
-      dataDirectory: path.join(process.cwd(), 'data'),
-      ...conf,
-    };
+    constructor(uin: number, conf?: Config) {
+        const config = {
+            logLevel: 'info' as LogLevel,
+            platform: Platform.Linux,
+            autoServer: true,
+            ignoreSelf: true,
+            cacheMember: true,
+            reConnInterval: 5,
+            dataDirectory: path.join(process.cwd(), 'data'),
+            ...conf,
+        };
 
-    const dir = createDataDir(config.dataDirectory, uin);
-    const deviceFile = path.join(dir, `device-${uin}.json`);
-    const tokenFile = path.join(dir, `token-${uin}.json`);
-    let regenerate, device, token;
-    try {
-      device = require(deviceFile) as DeviceInfo;
-      regenerate = false;
-    } catch {
-      device = generateDeviceInfo(uin);
-      regenerate = true;
-      fs.writeFileSync(deviceFile, JSON.stringify(device, null, 4));
+        const dir = createDataDir(config.dataDirectory, uin);
+        const deviceFile = path.join(dir, `device-${uin}.json`);
+        const tokenFile = path.join(dir, `token-${uin}.json`);
+        let regenerate, device, token;
+        try {
+            device = require(deviceFile) as DeviceInfo;
+            regenerate = false;
+        } catch {
+            device = generateDeviceInfo(uin);
+            regenerate = true;
+            fs.writeFileSync(deviceFile, JSON.stringify(device, null, 4));
+        }
+
+        try {
+            token = require(tokenFile) as SavedToken;
+        } catch {
+            token = null;
+        }
+        super(uin, token?.Uid ?? '', config.platform);
+        this.sig.signApiAddr = config.signApiAddr || this.sig.signApiAddr;
+        this.logger = log4js.getLogger(`[${this.deviceInfo.deviceName}:${uin}]`);
+        (this.logger as log4js.Logger).level = config.logLevel;
+        if (regenerate) this.logger.mark('创建了新的设备文件：' + deviceFile);
+        if (!token) this.logger.mark('未找到token缓存, 使用扫码登录');
+
+        this.directory = dir;
+        this.config = config as Required<Config>;
+        this.token =
+            token ??
+            ({
+                Uin: uin,
+                Uid: '',
+                PasswordMd5: '',
+                Session: {
+                    TempPassword: '',
+                },
+            } as SavedToken);
+
+        bindInternalListeners.call(this);
+        this.on('internal.verbose', (verbose, level) => {
+            const list: Exclude<LogLevel, 'off'>[] = ['fatal', 'mark', 'error', 'warn', 'info', 'trace'];
+            this.logger[list[level]](verbose);
+        });
+
+        if (!this.config.autoServer) this.setRemoteServer('msfwifi.3g.qq.com', 8080);
     }
 
-    try {
-      token = require(tokenFile) as SavedToken;
-    } catch {
-      token = null;
-    }
-    super(uin, token?.Uid ?? '', config.platform);
-    this.sig.signApiAddr = config.signApiAddr || this.sig.signApiAddr;
-    this.logger = log4js.getLogger(`[${this.deviceInfo.deviceName}:${uin}]`);
-    (this.logger as log4js.Logger).level = config.logLevel;
-    if (regenerate) this.logger.mark('创建了新的设备文件：' + deviceFile);
-    if (!token) this.logger.mark('未找到token缓存, 使用扫码登录');
-
-    this.directory = dir;
-    this.config = config as Required<Config>;
-    this.token =
-        token ??
-        ({
-          Uin: uin,
-          Uid: '',
-          PasswordMd5: '',
-          Session: {
-            TempPassword: '',
-          },
-        } as SavedToken);
-
-    bindInternalListeners.call(this);
-    this.on('internal.verbose', (verbose, level) => {
-      const list: Exclude<LogLevel, 'off'>[] = ['fatal', 'mark', 'error', 'warn', 'info', 'trace'];
-      this.logger[list[level]](verbose);
-    });
-
-    if (!this.config.autoServer) this.setRemoteServer('msfwifi.3g.qq.com', 8080);
-  }
-
-  /** emit an event */
-  em(name = '', data?: any) {
-    data = Object.defineProperty(data || {}, 'self_id', {
-      value: this.uin,
-      writable: true,
-      enumerable: true,
-      configurable: true,
-    });
-    while (true) {
-      this.emit(name, data);
-      const i = name.lastIndexOf('.');
-      if (i === -1) break;
-      name = name.slice(0, i);
-    }
-  }
-
-  async login(password?: string | Buffer) {
-    if (password && password.length > 0) {
-      let md5pass;
-      if (typeof password === 'string') md5pass = Buffer.from(password, 'hex');
-      else md5pass = password;
-      if (md5pass.length !== 16) md5pass = md5(String(password));
-
-      this.token.PasswordMd5 = md5pass.toString('hex');
+    /** emit an event */
+    em(name = '', data?: any) {
+        data = Object.defineProperty(data || {}, 'self_id', {
+            value: this.uin,
+            writable: true,
+            enumerable: true,
+            configurable: true,
+        });
+        while (true) {
+            this.emit(name, data);
+            const i = name.lastIndexOf('.');
+            if (i === -1) break;
+            name = name.slice(0, i);
+        }
     }
 
-    try {
-      const code = await this.tokenLogin(Buffer.from(this.token.Session.TempPassword, 'base64')); // EasyLogin
-      if (code) {
-        this.emit(`internal.error.token', "Token login failed, code: ${code}`);
-        return await this.passwordLogin(Buffer.from(this.token.PasswordMd5, 'hex'));
-      }
-    } catch (e) {
-      if (this.token.PasswordMd5 && this.token.Uid) {
-        // 检测Uid的目的是确保之前登陆过
-        return await this.passwordLogin(Buffer.from(this.token.PasswordMd5, 'hex'));
-      } else {
-        return await (this.sig.qrSig.length ? this.qrcodeLogin() : this.fetchQrcode());
-      }
+    async login(password?: string | Buffer) {
+        if (password && password.length > 0) {
+            let md5pass;
+            if (typeof password === 'string') md5pass = Buffer.from(password, 'hex');
+            else md5pass = password;
+            if (md5pass.length !== 16) md5pass = md5(String(password));
+
+            this.token.PasswordMd5 = md5pass.toString('hex');
+        }
+
+        try {
+            const code = await this.tokenLogin(Buffer.from(this.token.Session.TempPassword, 'base64')); // EasyLogin
+            if (code) {
+                this.emit(`internal.error.token', "Token login failed, code: ${code}`);
+                return await this.passwordLogin(Buffer.from(this.token.PasswordMd5, 'hex'));
+            }
+        } catch (e) {
+            if (this.token.PasswordMd5 && this.token.Uid) {
+                // 检测Uid的目的是确保之前登陆过
+                return await this.passwordLogin(Buffer.from(this.token.PasswordMd5, 'hex'));
+            } else {
+                return await (this.sig.qrSig.length ? this.qrcodeLogin() : this.fetchQrcode());
+            }
+        }
+
+        return LoginErrorCode.UnusualVerify;
     }
 
-    return LoginErrorCode.UnusualVerify;
-  }
+    async fetchClientKey() {
+        const response = await this.sendOidbSvcTrpcTcp(0x102a, 1, new Uint8Array(), false);
+        const packet = pb.decode(response);
 
-  async fetchClientKey() {
-    const response = await this.sendOidbSvcTrpcTcp(0x102a, 1, new Uint8Array(), false);
-    const packet = pb.decode(response);
+        return packet[4][3].toString();
+    }
+    sendOidbSvcTrpcTcp(cmd: number, subCmd: number, buffer: Uint8Array, isUid = false, isAfter = false) {
+        const command = `OidbSvcTrpcTcp.0x${cmd.toString(16)}_${subCmd}`;
 
-    return packet[4][3].toString();
-  }
-  sendOidbSvcTrpcTcp(cmd: number, subCmd: number, buffer: Uint8Array, isUid = false, isAfter = false) {
-    const command = `OidbSvcTrpcTcp.0x${cmd.toString(16)}_${subCmd}`;
-
-    const result = pb.encode({
-      1: cmd,
-      2: subCmd,
-      4: buffer,
-      7: isAfter
-          ? {
-            1: 0,
-            2: [],
-            3: this.appInfo.subAppId,
-          }
-          : null,
-      12: isUid,
-    });
-    return this.sendUni(command, result);
-  }
+        const result = pb.encode({
+            1: cmd,
+            2: subCmd,
+            4: buffer,
+            7: isAfter
+                ? {
+                    1: 0,
+                    2: [],
+                    3: this.appInfo.subAppId,
+                }
+                : null,
+            12: isUid,
+        });
+        return this.sendUni(command, result);
+    }
 }
 
 function createDataDir(dir: string, uin: number) {
-  if (!fs.existsSync(dir)) fs.mkdirSync(dir, { mode: 0o755, recursive: true });
-  const imgPath = path.join(dir, 'image');
-  const uinPath = path.join(dir, String(uin));
-  if (!fs.existsSync(imgPath)) fs.mkdirSync(imgPath);
-  if (!fs.existsSync(uinPath)) fs.mkdirSync(uinPath, { mode: 0o755 });
-  return uinPath;
+    if (!fs.existsSync(dir)) fs.mkdirSync(dir, { mode: 0o755, recursive: true });
+    const imgPath = path.join(dir, 'image');
+    const uinPath = path.join(dir, String(uin));
+    if (!fs.existsSync(imgPath)) fs.mkdirSync(imgPath);
+    if (!fs.existsSync(uinPath)) fs.mkdirSync(uinPath, { mode: 0o755 });
+    return uinPath;
 }
 
 export interface Logger {
-  trace(msg: any, ...args: any[]): any;
-  debug(msg: any, ...args: any[]): any;
-  info(msg: any, ...args: any[]): any;
-  warn(msg: any, ...args: any[]): any;
-  error(msg: any, ...args: any[]): any;
-  fatal(msg: any, ...args: any[]): any;
-  mark(msg: any, ...args: any[]): any;
+    trace(msg: any, ...args: any[]): any;
+    debug(msg: any, ...args: any[]): any;
+    info(msg: any, ...args: any[]): any;
+    warn(msg: any, ...args: any[]): any;
+    error(msg: any, ...args: any[]): any;
+    fatal(msg: any, ...args: any[]): any;
+    mark(msg: any, ...args: any[]): any;
 }
 
 export interface Config {
-  /** 日志等级，默认info (打印日志会降低性能，若消息量巨大建议修改此参数) */
-  logLevel?: LogLevel;
-  /** 1:Linux(Default) 2:MacOs 3:Windows*/
-  platform?: Platform;
-  /** 群聊和频道中过滤自己的消息(默认true) */
-  ignoreSelf?: boolean;
-  cacheMember?: boolean;
-  /** 数据存储文件夹，需要可写权限，默认主模块下的data文件夹 */
-  dataDirectory?: string;
-  /**
-   * 触发system.offline.network事件后的重新登录间隔秒数，默认5(秒)，不建议设置过低
-   * 设置为0则不会自动重连，然后你可以监听此事件自己处理
-   */
-  reConnInterval?: number;
-  /** 自动选择最优服务器(默认true)，关闭后会一直使用`msfwifi.3g.qq.com:8080`进行连接 */
-  autoServer?: boolean;
-  /** 签名API地址 */
-  signApiAddr?: string;
+    /** 日志等级，默认info (打印日志会降低性能，若消息量巨大建议修改此参数) */
+    logLevel?: LogLevel;
+    /** 1:Linux(Default) 2:MacOs 3:Windows*/
+    platform?: Platform;
+    /** 群聊和频道中过滤自己的消息(默认true) */
+    ignoreSelf?: boolean;
+    cacheMember?: boolean;
+    /** 数据存储文件夹，需要可写权限，默认主模块下的data文件夹 */
+    dataDirectory?: string;
+    /**
+     * 触发system.offline.network事件后的重新登录间隔秒数，默认5(秒)，不建议设置过低
+     * 设置为0则不会自动重连，然后你可以监听此事件自己处理
+     */
+    reConnInterval?: number;
+    /** 自动选择最优服务器(默认true)，关闭后会一直使用`msfwifi.3g.qq.com:8080`进行连接 */
+    autoServer?: boolean;
+    /** 签名API地址 */
+    signApiAddr?: string;
 }
 
 export interface SavedToken {
-  Uin: number;
-  Uid: string;
-  PasswordMd5: string;
-  Session: {
-    TempPassword: string;
-  };
+    Uin: number;
+    Uid: string;
+    PasswordMd5: string;
+    Session: {
+        TempPassword: string;
+    };
 }
 
 export type LogLevel = 'trace' | 'debug' | 'info' | 'warn' | 'error' | 'fatal' | 'mark' | 'off';
 export type Statistics = Client['statistics'];
 
 export function createClient(uin: number, config?: Config) {
-  if (isNaN(Number(uin))) throw new Error(uin + ' is not an QQ account');
-  return new Client(Number(uin), config);
+    if (isNaN(Number(uin))) throw new Error(uin + ' is not an QQ account');
+    return new Client(Number(uin), config);
 }
--- src/errors.ts
+++ src/errors.ts
@@ -1,70 +1,70 @@
 import { ApiRejection } from './core';
 
 export enum LoginErrorCode {
-  TokenExpired = 140022015,
-  UnusualVerify = 140022011,
-  NewDeviceVerify = 140022010,
-  CaptchaVerify = 140022008,
-  Success = 0
+    TokenExpired = 140022015,
+    UnusualVerify = 140022011,
+    NewDeviceVerify = 140022010,
+    CaptchaVerify = 140022008,
+    Success = 0
 }
 
 export enum ErrorCode {
-  /** 客户端离线 */
-  ClientNotOnline = -1,
-  /** 发包超时未收到服务器回应 */
-  PacketTimeout = -2,
+    /** 客户端离线 */
+    ClientNotOnline = -1,
+    /** 发包超时未收到服务器回应 */
+    PacketTimeout = -2,
 
-  /** 用户不存在 */
-  UserNotExists = -10,
-  /** 群不存在(未加入) */
-  GroupNotJoined = -20,
-  /** 群员不存在 */
-  MemberNotExists = -30,
+    /** 用户不存在 */
+    UserNotExists = -10,
+    /** 群不存在(未加入) */
+    GroupNotJoined = -20,
+    /** 群员不存在 */
+    MemberNotExists = -30,
 
-  /** 发消息时传入的参数不正确 */
-  MessageBuilderError = -60,
-  /** 群消息被风控发送失败 */
-  RiskMessageError = -70,
-  /** 群消息有敏感词发送失败 */
-  SensitiveWordsError = -80,
+    /** 发消息时传入的参数不正确 */
+    MessageBuilderError = -60,
+    /** 群消息被风控发送失败 */
+    RiskMessageError = -70,
+    /** 群消息有敏感词发送失败 */
+    SensitiveWordsError = -80,
 
-  /** 上传图片/文件/视频等数据超时 */
-  HighwayTimeout = -110,
-  /** 上传图片/文件/视频等数据遇到网络错误 */
-  HighwayNetworkError = -120,
-  /** 没有上传通道 */
-  NoUploadChannel = -130,
-  /** 不支持的file类型(没有流) */
-  HighwayFileTypeError = -140,
+    /** 上传图片/文件/视频等数据超时 */
+    HighwayTimeout = -110,
+    /** 上传图片/文件/视频等数据遇到网络错误 */
+    HighwayNetworkError = -120,
+    /** 没有上传通道 */
+    NoUploadChannel = -130,
+    /** 不支持的file类型(没有流) */
+    HighwayFileTypeError = -140,
 
-  /** 文件安全校验未通过不存在 */
-  UnsafeFile = -150,
+    /** 文件安全校验未通过不存在 */
+    UnsafeFile = -150,
 
-  /** 离线(私聊)文件不存在 */
-  OfflineFileNotExists = -160,
+    /** 离线(私聊)文件不存在 */
+    OfflineFileNotExists = -160,
 
-  /** 群文件不存在(无法转发) */
-  GroupFileNotExists = -170,
+    /** 群文件不存在(无法转发) */
+    GroupFileNotExists = -170,
 
-  /** 获取视频中的图片失败 */
-  FFmpegVideoThumbError = -210,
-  /** 音频转换失败 */
-  FFmpegPttTransError = -220,
+    /** 获取视频中的图片失败 */
+    FFmpegVideoThumbError = -210,
+    /** 音频转换失败 */
+    FFmpegPttTransError = -220,
 }
 
 const ErrorMessage: { [code: number]: string } = {
-  [ErrorCode.UserNotExists]: '查无此人',
-  [ErrorCode.GroupNotJoined]: '未加入的群',
-  [ErrorCode.MemberNotExists]: '幽灵群员',
-  [ErrorCode.RiskMessageError]: '群消息发送失败，可能被风控',
-  [ErrorCode.SensitiveWordsError]: '群消息发送失败，请检查消息内容',
-  10: '消息过长',
-  34: '消息过长',
-  120: '在该群被禁言',
-  121: 'AT全体剩余次数不足',
+    [ErrorCode.UserNotExists]: '查无此人',
+    [ErrorCode.GroupNotJoined]: '未加入的群',
+    [ErrorCode.MemberNotExists]: '幽灵群员',
+    [ErrorCode.RiskMessageError]: '群消息发送失败，可能被风控',
+    [ErrorCode.SensitiveWordsError]: '群消息发送失败，请检查消息内容',
+    10: '消息过长',
+    34: '消息过长',
+    120: '在该群被禁言',
+    121: 'AT全体剩余次数不足',
 };
 
 export function drop(code: number, message?: string): never {
-  if (!message || !message.length) message = ErrorMessage[code];
-  throw new ApiRejection(code, message);
+    if (!message || !message.length) message = ErrorMessage[code];
+    throw new ApiRejection(code, message);
 }
